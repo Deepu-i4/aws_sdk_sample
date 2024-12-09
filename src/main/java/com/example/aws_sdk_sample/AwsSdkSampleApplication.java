4ff46b364db3a9c2be9afad69f3926aba0fca94c
deepu <pushpendra.iimt08@gmail.com>
2024-12-10 01:04:43 +0530

    changes

---
+++ src/main/java/com/example/aws_sdk_sample/AwsSdkSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_sdk_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSdkSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSdkSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aws_sdk_sample/AwsSdkSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_sdk_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSdkSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
